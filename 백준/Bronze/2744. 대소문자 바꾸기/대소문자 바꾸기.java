6cb18092f70867a75822edf15d0ca96ef96dc472
JungSun Shin <104877529+SHINJUNGSUN@users.noreply.github.com>
2025-03-10 12:10:09 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 172 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for(char c : str.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        System.out.println(sb);
+    }
+}
