52d60acb50140f80daed62fd8bad97f7f0ffa057
JungSun Shin <104877529+SHINJUNGSUN@users.noreply.github.com>
2025-03-19 20:07:50 +0900

    [Silver III] Title: 개미, Time: 124 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -1,19 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int W = sc.nextInt();
-        int H = sc.nextInt();
-        int P = sc.nextInt();
-        int Q = sc.nextInt();
-        int T = sc.nextInt();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String[] box = reader.readLine().split(" ");
+            String[] current = reader.readLine().split(" ");
+            String count = reader.readLine();
+            int W = Integer.parseInt(box[0]);
+            int H = Integer.parseInt(box[1]);
+            int P = Integer.parseInt(current[0]);
+            int Q = Integer.parseInt(current[1]);
+            int T = Integer.parseInt(count);
 
-        int currentX = (T + P) % (2 * W);
-        int currentY = (T + Q) % (2 * H);
-        if (currentX > W) currentX = 2 * W - currentX;
-        if (currentY > H) currentY = 2 * H - currentY;
+            int currentX = (T + P) % (2 * W);
+            int currentY = (T + Q) % (2 * H);
+            if (currentX > W) currentX = 2 * W - currentX;
+            if (currentY > H) currentY = 2 * H - currentY;
 
-        System.out.println(currentX + " " + currentY);
+            System.out.println(currentX + " " + currentY);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
